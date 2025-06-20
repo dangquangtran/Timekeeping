f422b305e6fece1efa652446b7ff02c404e2a98b
QuangLaTui <quangtran2002qn@gmail.com>
2025-06-20 14:00:01 +0700

    done phan quyen

---
+++ Timekeeping/wwwroot/js/phan-quyen.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+    $('#filter-msnv').select2({
+        placeholder: "Tất cả",
+        allowClear: true,
+        width: 'resolve',
+        language: {
+            noResults: function () {
+                return "Không tìm thấy";
+            }
+        }
+    });
+    $('#select-mans').select2({
+        placeholder: "Chọn mã nhân sự",
+        allowClear: true,
+        width: 'resolve',
+        language: {
+            noResults: function () {
+                return "Không tìm thấy";
+            }
+        }
+    });
+    $('#select-nhomnd').select2({
+        placeholder: "Chọn nhóm ND",
+        allowClear: true,
+        width: 'resolve',
+        language: {
+            noResults: function () {
+                return "Không tìm thấy";
+            }
+        }
+    });
+    $('.form-create').on('reset', function () {
+        setTimeout(function () {
+            $('#select-mans').val('').trigger('change');
+            $('#select-nhomnd').val('').trigger('change');
+        }, 0);
+    });
+});
